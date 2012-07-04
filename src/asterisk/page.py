e09160ff9a3e193142c224e944f4b66222268b13
Dhananjay Balan <mb.dhananjay@gmail.com>
2012-07-04 14:19:03 +0530

    Design guidelines.

--- src/asterisk/page.py
+++ src/asterisk/page.py
@@ -2,5 +2,21 @@
 # Implements the configration of the paging chanels.
 # Class channels
 # 	methods:
+#       - __init__ -> returns the current database structure for printing.
 #		- create(chanelno, [chanels]..,password)
 #		- delete(chanelno)
+#		- update() - syncs the database.
+
+
+class page:
+	def __init__:
+		print "Intialized!"
+
+	def create(id, clients,password):
+		print "Created the channel",id
+
+	def delete(id):
+		print "chanel "+str(id)+" is deleted"
+
+	def update():
+		print "Sync"
