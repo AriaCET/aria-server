d85052d8564be4119f580ab3e9ad012476a4ba99
Dhananjay Balan <mb.dhananjay@gmail.com>
2012-07-04 14:08:42 +0530

    Backend skeleton.

--- src/asterisk/clients.py
+++ src/asterisk/clients.py
@@ -1,1 +1,8 @@
+#
+# Implements the client configuration.
+# provides the foillowing.
+# class sip 
+#	methods
+#	- create(username,password)
+#	- delete(id) 
 
---
+++ src/asterisk/page.py
@@ -0,0 +1,6 @@
+#
+# Implements the configration of the paging chanels.
+# Class channels
+# 	methods:
+#		- create(chanelno, [chanels]..,password)
+#		- delete(chanelno)
