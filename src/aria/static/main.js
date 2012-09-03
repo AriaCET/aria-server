e884a90a6816ef91a774b3f5d9bd131c778c82ae
Thomas <methomasabraham@gmail.com>
2012-09-03 14:33:43 +0530

    Replaced crappy javascript code with standard code.
    SetTimeout has been used in several places to reload a page after posting data which should be done using call back of post in jquery. This crappy code is replaced with proper callback code. This will prevent the system from crashing in slow network.

--- src/aria/static/main.js
+++ src/aria/static/main.js
@@ -2,29 +2,23 @@ function addit()
 {
     var channel_id = $('#inputchannelid').val();
     var channel_name = $('#inputchannelname').val();
-    $.post('/addchannel/',{channelid:channel_id ,channelname:channel_name});
-    $('#SpeakersInChannelBlock').html(" ");
-    setTimeout(
-	function(){
-	    $('#main_block').load('/channelmanager');
-	}
-	,500);
+    $.post('/addchannel/',{channelid:channel_id ,channelname:channel_name},
+	   function(data){
+	       $('#SpeakersInChannelBlock').html(" ");
+	       $('#main_block').load('/channelmanager');
+	   });
 }
 function removeFromChannel(speaker, channel){
-    $.post('/removefromchannel/',{clientid:speaker,groupid:channel});
-    setTimeout(
-	function(){
-	    editChannel(channel);
-	}
-	,500);
+    $.post('/removefromchannel/',{clientid:speaker,groupid:channel},
+	   function(data){
+	       editChannel(channel);   
+	   });
 }
 function addToChannel(speaker, channel){
-    $.post('/addtochannel/',{clientid:speaker,groupid:channel});
-    setTimeout(
-	function(){
-	    editChannel(channel);
-	}
-	,500);    
+    $.post('/addtochannel/',{clientid:speaker,groupid:channel},
+	   function(data){
+	       editChannel(channel);
+	   });
 }
 function editChannel(channel){
     $('#SpeakersInChannelBlock').load('/listchannel/'+channel);
@@ -36,12 +30,10 @@ function channelManager(){
     $('#main_block').load('/channelmanager');
 }
 function deleteChannel(channelid){
-    $.post('/removechannel/',{channel:channelid});
-    setTimeout(
-	function(){
-	    $('#main_block').load('/channelmanager');
-	}
-	,500);
+    $.post('/removechannel/',{channel:channelid},
+	   function(data){
+	       $('#main_block').load('/channelmanager');
+	   });
 }
 function reloadDialplan(){
     $.get('/reloaddialplan/');
