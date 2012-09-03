0866059a884812c98ca988688e708225547c4382
Thomas <methomasabraham@gmail.com>
2012-09-03 15:23:21 +0530

    Dynamic speaker addition using jquery.
    When new speakers are added to the system the entire page was realoding since we implemented it using html forms. Now the html form is replaced with jquery code, which dynamically adds the new speaker and makes form validation in future easier.

--- src/aria/static/main.js
+++ src/aria/static/main.js
@@ -1,22 +1,35 @@
-function addit()
-{
+function addChannel(){
     var channel_id = $('#inputchannelid').val();
     var channel_name = $('#inputchannelname').val();
+    //todo: add form validation.
     $.post('/addchannel/',{channelid:channel_id ,channelname:channel_name},
 	   function(data){
-	       $('#SpeakersInChannelBlock').html(" ");
-	       $('#main_block').load('/channelmanager');
+	       //todo: check for server errors.
+	       channelManager();
+	   });
+}
+function addSpeaker(){
+    var speaker_number = $('#inputspeakernumber').val();
+    var speaker_name = $('#inputspeakername').val();
+    var speaker_ip = $('#inputspeakerip').val();
+    //todo: add form validation.
+    $.post('/addspeaker/',{number:speaker_number,name:speaker_name,ip:speaker_ip},
+	   function(data){
+	       //todo: check for server errors.
+	       speakerManager();
 	   });
 }
 function removeFromChannel(speaker, channel){
     $.post('/removefromchannel/',{clientid:speaker,groupid:channel},
 	   function(data){
+	       //todo: check for server errors.
 	       editChannel(channel);   
 	   });
 }
 function addToChannel(speaker, channel){
     $.post('/addtochannel/',{clientid:speaker,groupid:channel},
 	   function(data){
+	       //todo: check for server errors.
 	       editChannel(channel);
 	   });
 }
@@ -32,6 +45,7 @@ function channelManager(){
 function deleteChannel(channelid){
     $.post('/removechannel/',{channel:channelid},
 	   function(data){
+	       //todo: check for server errors.
 	       $('#main_block').load('/channelmanager');
 	   });
 }
