9328d1c3501502b8f294cd8b212c32770ccfb6f0
Dhananjay Balan <mb.dhananjay@gmail.com>
2012-09-10 13:04:07 +0530

    Fixed bug: Channel configuration not created.

--- src/aria/asterisk/config.py
+++ src/aria/asterisk/config.py
@@ -5,4 +5,4 @@ default_path = "aria"
 DB_path = os.path.join(default_path,"ariaDB")
 
 clientConf = os.path.join(asteriskconf,'sip.conf')
-channelConf = os.path.join(asteriskconf,'extentions.conf')
+channelConf = os.path.join(asteriskconf,'extensions.conf')
