4f38516d69ff95629e1100ceb624e3e9bb6326c1
abilng <abilngeorge@gmail.com>
2013-02-13 18:57:39 +0530

    Added:Install script.

---
+++ asterisk-conf/config.py
@@ -0,0 +1,5 @@
+from os.path import join 
+asteriskconf='/etc/asterisk'
+DB_path = "/etc/aria-server/ariaDB"
+clientConf = join(asteriskconf,'sip.conf')
+channelConf = join(asteriskconf,'extensions.conf')
